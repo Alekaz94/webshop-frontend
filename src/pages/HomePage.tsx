a01997cf0343791f148508fcd5dd42988d60e80e
alexandroskazalis <alexandros.kazalis@hotmail.com>
2024-07-14 17:14:37 +0200

    Created a footer with some contact and about us info

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,16 @@
+import Footer from "../public-components/Footer";
 import Navbar from "../public-components/Navbar";
+import "../styles/HomePageStyles.css"
 
 function HomePage() {
 
     return (
-        <>
+        <>               
             <Navbar />
-        
+            <div className="homepage-body">    
+
+            </div>
+            <Footer />
         </>
     )
 }
